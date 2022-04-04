7321a8eb0877474e98bae924e68639f7df3f18ec
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-04-04 10:36:33 +0200

    lite mer test

--- src/components/booking.tsx
+++ src/components/booking.tsx
@@ -121,32 +121,159 @@ export function Booking() {
             "time": "18:00",
             "numberOfGuests": 4,
             "customerId": "623b85d54396b96c57bde7c3"
+        },
+
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
+        },
+        {
+            "id": "623b85d54396b96c57bde7c3",
+            "restaurantId": "623b85d54396b96c57bde7c3",
+            "date": "2022-01-01",
+            "time": "21:00",
+            "numberOfGuests": 4,
+            "customerId": "623b85d54396b96c57bde7c3"
         }
+
 
     ]
 
-    function checkIfOpenTable(){
+    function checkIfOpenTable() {
         console.log("testar");
 
-        let chosenDate:string = "2022-01-01"
+        let chosenDate: string = "2022-01-01"
 
-        let numberOfTablesLeft:number = 15
+        let numberOfTablesAt6Left: number = 15
+        let numberOfTablesAt9Left: number = 15
 
         for (let i = 0; i < mockData.length; i++) {
             const order = mockData[i];
 
-            if (order.date === chosenDate) {
-                numberOfTablesLeft --
-                console.log(numberOfTablesLeft);
-                
+            if (order.date === chosenDate && order.time === "18:00") {
+                numberOfTablesAt6Left--
+                console.log(numberOfTablesAt6Left);
+
+            } else if(order.date === chosenDate && order.time === "21:00") {
+                numberOfTablesAt9Left--
+                console.log(numberOfTablesAt9Left);
+
             }
-            
+
         }
 
-        console.log("här fanns det " + numberOfTablesLeft + " bord kvar");
-        
-        
-       
+        console.log("här fanns det " + numberOfTablesAt6Left + " bord kvar kl 18 och " + numberOfTablesAt9Left + " bord kvar kl 21");
+
+
+
     }
 
 
