3f680576e89a73039b4f7f8a3b87d5f931e63330
thereseml <tm-l@live.se>
2022-04-06 10:11:27 +0200

    början på kontaktfomruläar

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react"
+import { text } from "stream/consumers";
+
 export function Contact(){
+
+    interface INewContact {
+        firstname: string;
+        lastname: string;
+        email: string;
+        message: string;
+    }
+
+    const [newContact, setNewContact] = useState<INewContact>({
+        firstname: '',
+        lastname: '',
+        email: '',
+        message: '',
+    });
+
+
+
     return (<>
-        <div>Contact Works</div>
+        <h2>Kontakta oss</h2>
+        <form>
+            <label>Förnamn:</label>
+            <input type="text" value={newContact.firstname}></input>
+            <label>Efternamn:</label>
+            <input type="text" value={newContact.lastname}></input>
+            <label>E-post:</label>
+            <input type="email" value={newContact.email}></input>
+            <label>Meddelande:</label>
+            <input type="text" value={newContact.message}></input>
+            <button type="submit" >Skicka meddelande!</button>
+        </form>
     </>)
 }
