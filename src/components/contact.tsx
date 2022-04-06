ddbf0588f19dff1f9549548b7846d064a7ffcbd3
thereseml <tm-l@live.se>
2022-04-06 10:46:26 +0200

    fomulär uppdate

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { text } from "stream/consumers";
 
 export function Contact(){
@@ -17,20 +17,33 @@ export function Contact(){
         message: '',
     });
 
+    const [messageSend, setMessageSend] = useState(false)
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        let name = e.target.name;
+        setNewContact({...newContact, [name]: e.target.value})
+    }
+
+    function handleSubmit(e: any) {
+        setMessageSend(true)
+        e.preventDefault()
+        
+    }
 
 
     return (<>
         <h2>Kontakta oss</h2>
-        <form>
+        <form onSubmit={handleSubmit}> 
             <label>Förnamn:</label>
-            <input type="text" value={newContact.firstname}></input>
+            <input type="text" name="firstname" value={newContact.firstname} onChange={handleChange}></input>
             <label>Efternamn:</label>
-            <input type="text" value={newContact.lastname}></input>
+            <input type="text" name="lastname" value={newContact.lastname} onChange={handleChange}></input>
             <label>E-post:</label>
-            <input type="email" value={newContact.email}></input>
+            <input type="email" name="email" value={newContact.email} onChange={handleChange}></input>
             <label>Meddelande:</label>
-            <input type="text" value={newContact.message}></input>
+            <input type="text" name="message" value={newContact.message} onChange={handleChange}></input>
             <button type="submit" >Skicka meddelande!</button>
+            {messageSend && <h3>Ditt meddelande är skickat!</h3>}
         </form>
     </>)
 }
