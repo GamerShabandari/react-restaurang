34073d6c79d1ddde994f5d5c75567afaf1c2067f
Husse00 <94406339+Husse00@users.noreply.github.com>
2022-04-06 23:09:38 +0200

    rendering data

--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -1,5 +1,42 @@
-export function Admin() {
-    return (<>
-        <div>Admin Works</div>
-    </>);
+import { useState } from "react";
+import BookingDataAdmin from "../data/BookingDataAdmin";
+import BookingList from "./BookingList"
+
+interface Adminprops{
+text: string
+bgColor: string
+textColor: string
 }
+
+
+export function Admin({ text, bgColor, textColor }: Adminprops) {
+
+  const Adminstyles = {
+    backgroundColor: bgColor, color: textColor
+  }
+
+  const [booking, setBooking] = useState (BookingDataAdmin)
+  return (
+    <header style={Adminstyles}>
+      
+        <h2>{text}</h2>
+        <BookingList booking={booking}/>
+      
+        
+      
+    </header>
+    
+  )
+}
+
+Admin.defaultProps = {
+  text: 'ADMIN',
+  bgColor: 'rgba(0,0,0,0.0)',
+  textColor: 'rgb(236, 85, 85)',
+  }
+  
+
+
+
+
+
---
+++ src/data/BookingDataAdmin.tsx
@@ -0,0 +1,24 @@
+const BookingDataAdmin = [
+    {
+    id: 1,
+    table: 8,
+    text: 'lorem ipsum',
+    },
+    {id: 3000,
+        table: 4,
+        text: 'lorem ipsum',},
+    
+    {id: 3,
+    table: 2,
+    text: 'lorem ipsum',},
+    
+    {id: 4,
+        table: 9,
+        text: 'lorem ipsum',},
+    
+    {id: 5,
+     table: 9,
+     text: 'lorem ipsum',}
+    ]
+    
+    export default BookingDataAdmin
