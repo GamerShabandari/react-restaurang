79425a3949785fcca0f5c22ed849a8ec72823030
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-04-10 12:02:43 +0200

    Revert "Home and Footer komponent är klar. "

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Home } from './components/home';
 import { Booking } from './components/booking';
 import { Contact } from './components/contact';
 import { Admin } from './components/admin';
-import { Footer} from './components/footer';
 import { GiSamuraiHelmet, GiSushis } from "react-icons/gi";
 import { MdOutlineHome, MdCall, MdOutlineAdminPanelSettings } from "react-icons/md"
 import "animate.css"
@@ -27,8 +26,8 @@ function App() {
                 <li> <Link to="/admin">Admin<MdOutlineAdminPanelSettings></MdOutlineAdminPanelSettings></Link></li>
               </ul>
             </div>
+
           </nav>
-          <p style={{ backgroundImage: "url(images/katana.png)"}}></p>
         </header>
         <Routes>
           <Route path='/' element={<Home></Home>}> </Route>
@@ -40,10 +39,7 @@ function App() {
           <Route path='/admin' element={<Admin></Admin>}> </Route>
 
         </Routes>
-
       </BrowserRouter>
-
-      <Footer></Footer>
 
     </div>
   );
