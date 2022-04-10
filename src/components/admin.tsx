eb722a23e16544a68700d711fae02fb48a3fec50
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-04-10 20:31:16 +0200

    fixat det mesta som behövs på admin nu, kan radera bookings samt se detaljer, kvarstår endast kunna ändra en bokning

--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -4,16 +4,31 @@ import { IBooking } from "./models/IBooking"
 
 import "./admin.css"
 import { INewUser } from "./models/INewUser"
+import { Bookings } from "./models/Bookings"
 
 export function Admin() {
 
 
     const [bookingsFromApi, setBookingsFromApi] = useState<IBooking[]>([])
-    const [testUser, setTestuser] = useState<INewUser>({
-        name: "hallo",
+    const [showDetailsSection, setShowDetailsSection] = useState(false);
+    const [customer, setCustomer] = useState<INewUser>({
+        name: "",
         lastname: "",
         email: "",
         phone: ""
+    })
+
+    const [detailedBooking, setDetailedBooking] = useState<Bookings>({
+        restaurantId: "",
+        date: "",
+        time: "",
+        numberOfGuests: 0,
+        customer: {
+            name: "",
+            lastname: "",
+            email: "",
+            phone: ""
+        }
     })
 
 
@@ -30,19 +45,19 @@ export function Admin() {
 
         let chosenBooking = bookingsFromApi[bookingIndex];
 
-        axios.get<INewUser>("https://school-restaurant-api.azurewebsites.net/customer/" + chosenBooking.customerId)
+        axios.get<INewUser[]>("https://school-restaurant-api.azurewebsites.net/customer/" + chosenBooking.customerId)
             .then(response => {
-                console.log(response.data);
 
                 let user: INewUser = {
-                    name: response.data.name,
-                    lastname: response.data.lastname,
-                    email: response.data.email,
-                    phone: response.data.phone
+                    name: response.data[0].name,
+                    lastname: response.data[0].lastname,
+                    email: response.data[0].email,
+                    phone: response.data[0].phone
                 }
-                //     console.log(user);
-
-                //   setTestuser(user) 
+                setCustomer(user)
+                let completeBooking = new Bookings(chosenBooking.restaurantId, chosenBooking.date, chosenBooking.time, chosenBooking.numberOfGuests, user);
+                setDetailedBooking(completeBooking)
+                setShowDetailsSection(true)
             })
     }
 
@@ -58,20 +73,36 @@ export function Admin() {
             })
     }
 
+    function closeDetailsSection() {
+        setShowDetailsSection(false)
+    }
+
     let bookingsHtml = bookingsFromApi.map((booking, i) => {
         return (<div className="bookingBox" key={i}>
-            <h3>{booking.date}</h3>
-            <h4>{booking.time}</h4>
-            <h4>{booking.numberOfGuests}</h4>
-            <h5>{booking._id}</h5>
+            <h5>Bokningsnr: {booking._id}</h5>
+            <h3>Datum: {booking.date}</h3>
+            <h4>Tid: {booking.time}</h4>
+            <h4>Antal gäster:{booking.numberOfGuests}</h4>
             <button onClick={() => { showDetails(i) }}>se detailjer</button>
             <button onClick={() => { deleteBooking(booking._id, i) }}>ta bort bokning</button>
         </div>)
     })
 
+    let detailsHtml = (
+        <div className="detailsBox">
+            <button onClick={closeDetailsSection}>stäng</button>
+            <h2>Kund: {customer.name} {customer.lastname}</h2>
+            <h3>Epost: {customer.email}</h3>
+            <h3>Telefon: {customer.phone}</h3>
+            <h5>Antal gäster: {detailedBooking.numberOfGuests}</h5>
+            <h5>Datum: {detailedBooking.date}</h5>
+            <h5>Tid: {detailedBooking.time}</h5>
+        </div>)
+
     return (<>
         <main className="adminMain">
-            <h1>{testUser.name}</h1>
+            {showDetailsSection && <section className="adminDetailsContainer">{detailsHtml}</section>}
+
             {bookingsFromApi.length < 1 && <div>laddar...</div>}
             {bookingsFromApi.length > 0 && <section className="adminBookingsContainer">{bookingsHtml}</section>}
         </main>
